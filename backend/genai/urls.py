dc3c812f2e9b9e0019432850082b35e45435499f
wletshu <123249866+William-Letshu@users.noreply.github.com>
2026-02-09 04:21:07 +0100

    13 backend auth (#16)
    * WIP: init user auth
    
    * WIP: added user app to manage creation of a new user and store it in database
    
    * WIP: Added user auth, disable email verification and added media path for pictures

--- backend/genai/urls.py
+++ backend/genai/urls.py
@@ -21,11 +21,11 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path('api/auth/', include('accounts.urls')),
 ]
